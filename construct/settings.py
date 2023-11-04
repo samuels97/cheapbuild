2ad3276573369e2e6207cb629b694ca2807d0442
bradiepoa7@gmail.com <bradiepoa7@gmail.com>
2023-11-04 13:00:54 +0300

    modified:   construct/settings.py

--- construct/settings.py
+++ construct/settings.py
@@ -125,12 +125,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
 
 # config media 
 MEDIA_URL = '/media/'
