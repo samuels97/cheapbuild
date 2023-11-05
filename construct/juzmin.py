dcfaba304b1c56890c532632d9b29277e31bc88a
bradiepoa7@gmail.com <bradiepoa7@gmail.com>
2023-11-05 17:53:14 +0300

    modified:   construct/juzmin.py 	modified:   static/css/bootstrap.min.css

--- construct/juzmin.py
+++ construct/juzmin.py
@@ -8,7 +8,7 @@ JAZZMIN_SETTINGS = {
     "site_brand": "CHEAP BUILD",
 
     # Logo to use for your site, must be present in static files, used for brand on top left
-    "site_logo": "img/logo.png",
+    "site_logo": "img/logo.jpeg",
 
     # CSS classes that are applied to the logo above
     "site_logo_classes": "img-circle",
